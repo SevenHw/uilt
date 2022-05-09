e777e6bef893a6b6b96a6ed8faca98e6f742a7ab
like <like@zto56.com>
2022-05-09 21:42:25 +0800

    提交工具类

---
+++ look-common/src/main/java/com/look/common/util/Message.java
@@ -0,0 +1,54 @@
+package com.look.common.util;
+
+import java.util.HashMap;
+import java.util.Map;
+/**
+ * @ClassName Message
+ * @Author like
+ * @Date 2022/5/9 21:08
+ **/
+public class Message {
+    /**
+     * 响应状态码，约定200成功，失败500
+     **/
+    private Integer code;
+    /**
+     * 代表响应信息
+     **/
+    private String msg;
+    private Map<String, Object> info=new HashMap<>();
+    public static Message success() {
+        Message message=new Message();
+        message.setCode(200);
+        message.setMsg("成功");
+        return message;
+    }
+    public static Message fail() {
+        Message message=new Message();
+        message.setCode(500);
+        message.setMsg("失败");
+        return message;
+    }
+    public Message addInfo(String key,Object value) {
+        getInfo().put(key, value);
+        return this;
+    }
+    public Integer getCode() {
+        return code;
+    }
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+    public String getMsg() {
+        return msg;
+    }
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+    public Map<String, Object> getInfo() {
+        return info;
+    }
+    public void setInfo(Map<String, Object> info) {
+        this.info = info;
+    }
+}
