313b59ff61bac478489cc0fb22f402dec907f553
like <1183350012@qq.com>
2022-05-12 17:24:17 +0800

    集成es

--- look-common/src/main/java/com/look/common/config/mybatisConfig/GeneratorCodeConfig.java
+++ look-common/src/main/java/com/look/common/config/mybatisConfig/GeneratorCodeConfig.java
@@ -1,4 +1,4 @@
-package com.look.common.util.mybatisUtil;
+package com.look.common.config.mybatisConfig;
 
 import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
 import com.baomidou.mybatisplus.core.toolkit.StringUtils;
--- look-common/src/main/java/com/look/common/config/mybatisConfig/MybatisPlusConfig.java
+++ look-common/src/main/java/com/look/common/config/mybatisConfig/MybatisPlusConfig.java
@@ -1,4 +1,4 @@
-package com.look.common.util.mybatisUtil;
+package com.look.common.config.mybatisConfig;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.springframework.context.annotation.Bean;
